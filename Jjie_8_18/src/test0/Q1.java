bab21de6c517ff2fd9d06f6c07c58add5fdbd6b2
Jjie <oyjj20264@gmail.com>
2025-08-18 14:59:11 +0800

    08/17(2)

---
+++ Jjie_8_18/src/test0/Q1.java
@@ -0,0 +1,15 @@
+package test0;
+class ListNode{
+    public ListNode next;
+    public int val;
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+public class Q1 {
+    public boolean isPalindrome(ListNode head) {
+        int len=0;
+    }
+}
