19000737b3767b9516e287fed7aa8822cd59bcd7
Jjie <oyjj20264@gmail.com>
2025-08-25 10:54:56 +0800

    08/24(1)

---
+++ Jjie_8_24/src/test0/Q1.java
@@ -0,0 +1,14 @@
+package test0;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+public class Q1 {
+    public ListNode mergeKLists(ListNode[] lists) {
+
+    }
+}
